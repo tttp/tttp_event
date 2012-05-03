73ec5130580f88729b97dfb675f8295c69340f01
xavier dutoit <git@tttp.eu>
2012-05-03 15:32:43 +0200

    update from dev new qrcode

--- api/v3/Participant/GetFull.php
+++ api/v3/Participant/GetFull.php
@@ -1,7 +1,7 @@
 <?
 
 function civicrm_api3_participant_getfull ($param) {
-  $query= "SELECT civicrm_participant.id, event_id,civicrm_participant.contact_id,status_id, role_id,first_name,last_name, job_title, organization_name, employer_id, civicrm_address.id as address_id, civicrm_country.id as country_id, civicrm_country.name as country
+  $query= "SELECT civicrm_participant.id, event_id,civicrm_participant.contact_id,status_id, role_id,first_name,last_name, job_title, organization_name, employer_id, civicrm_address.id as address_id, civicrm_country.id as country_id, civicrm_country.name as country, email, civicrm_email.id as email_id
     FROM civicrm_participant 
     JOIN civicrm_contact on civicrm_contact.id= civicrm_participant.contact_id 
     LEFT JOIN civicrm_address on civicrm_address.contact_id = civicrm_contact.id and is_primary=1 
